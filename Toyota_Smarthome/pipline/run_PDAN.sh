2a898d5c5e15b7bae34de3697f1af1be63b1ddb9
cweiwenr <reubenchongww2512@gmail.com>
2022-09-12 15:00:00 +0800

    TEAM01V2-7 Team01-7 choose feature data from TSU project

---
+++ Toyota_Smarthome/pipline/run_PDAN.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+export PATH=/home/rdai/anaconda3/envs/torch1.9/bin:$PATH
+
+python ./Toyota_Smarthome/pipline/train.py \
+-dataset TSU \
+-mode rgb \
+-split_setting CS \
+-model ./trained_model/$1 \
+-train True \
+-num_channel 512 \
+-lr 0.0002 \
+-kernelsize 3 \
+-APtype map \
+-epoch 140 \
+-batch_size 2 \
+-comp_info TSU_CS_RGB_PDAN \
+-data $2 \
+
