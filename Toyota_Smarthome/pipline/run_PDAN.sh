9f6fde2ca844c4d1b9c2f073b18791989f7f4274
cweiwenr <reubenchongww2512@gmail.com>
2022-09-18 13:38:42 +0800

    clean up code

--- Toyota_Smarthome/pipline/run_PDAN.sh
+++ Toyota_Smarthome/pipline/run_PDAN.sh
@@ -6,7 +6,7 @@ python ./Toyota_Smarthome/pipline/train.py \
 -dataset TSU \
 -mode rgb \
 -split_setting CS \
--model ./trained_model/$1 \
+-model PDAN \
 -train True \
 -num_channel 512 \
 -lr 0.0002 \
@@ -15,6 +15,7 @@ python ./Toyota_Smarthome/pipline/train.py \
 -epoch ${3:-1} \
 -batch_size ${4:-1} \
 -comp_info TSU_CS_RGB_PDAN \
--data $2 \
--newModel ${5:-.}
+-data $1 \
+-load_model ${2:-'false'} \
+-save_model ${5}
 
